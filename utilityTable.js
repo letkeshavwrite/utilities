baf8fe6bf6e9c9f009813670b0d70ac0e0dce601
keshav <keshavp@appcino.com>
2022-09-09 16:21:06 +0530

    Changes in js file

--- utilityTable.js
+++ utilityTable.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class UtilityTable extends LightningElement {
-    
+    connectedCallback(){
+        console.log('checking git');
+    }
 }
